c76c2e414aee2b70b0313610af28fccb2b48d8e6
AJAY kumar <rajaykumar1111@gmail.com>
2025-07-20 09:47:37 +0000

    Make it 10ms

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -31,8 +31,8 @@ export default function Hero() {
 
   const y = useTransform(scrollYProgress, [0, 1], ["0%", "10%"]);
 
-  const typedTitle = useTypingEffect("Marketing Science & Data Professional", 120);
-  const typedSubtitle = useTypingEffect("Streamlining DataOps Workflows", 100, 4500); // Start after a delay
+  const typedTitle = useTypingEffect("Marketing Science & Data Professional", 10);
+  const typedSubtitle = useTypingEffect("Streamlining DataOps Workflows", 10, 4500); // Start after a delay
 
   return (
     <section ref={targetRef} id="home" className="relative w-full min-h-screen flex flex-col items-center justify-center overflow-hidden py-24 sm:py-16">
